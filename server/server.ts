afab8529ab63956078e921fb9a0548c93cdf22e1
n8ngo <nathanngo8796@gmail.com>
2023-04-17 15:57:30 -0700

    setup server Co-authored-by: b-v-u <briandoanvu@gmail.com> Co-authored-by: n8ngo <nathanngo8796@gmail.com> Co-authored-by: Jonathan Klibansky <jon19200@gmail.com> Co-authored-by: David Kim <david.toyou.kim@gmail.com>

---
+++ server/server.ts
@@ -0,0 +1,42 @@
+import express, { Request, Response, NextFunction } from 'express';
+import dotenv from 'dotenv';
+const path = require('path');
+// const db = require('./models/dbConnect.js');
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.resolve(__dirname, '../client')));
+
+/**
+ * define route handlers
+ */
+
+// app.use('/solo', middleware);
+
+// catch-all route handler
+app.use((req, res) => res.status(404).send('PAGE NOT FOUND!'));
+
+// Global error handler
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  // console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+/**
+ * start server
+ */
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}...`);
+});
+
+module.exports = app;
