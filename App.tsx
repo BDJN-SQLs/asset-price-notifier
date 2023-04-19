d5cf3adedf208d77e9da85cab416f95cce33dee9
davidtoyoukim <david.toyou.kim@gmail.com>
2023-04-19 10:20:47 -0700

    Co-authored-by: b-v-u <briandoanvu@gmail.com> Co-authored-by: n8ngo <nathanngo8796@gmail.com> Co-authored-by: Jonathan Klibansky <jon19200@gmail.com> Co-authored-by: David Kim <david.toyou.kim@gmail.com>

--- App.tsx
+++ App.tsx
@@ -1,48 +1,44 @@
-// import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import Dropdown from "./components/dropDown";
+import StockInfo from "./components/stockInfo";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+const { Navigator, Screen } = createNativeStackNavigator();
 
 // export default function App() {
 //   return (
 //     <View style={styles.container}>
+//       <Dropdown />
 //       <Text>Open up App.tsx to start working on your app!</Text>
 //     </View>
 //   );
 // }
 
-import React from "react";
-import { View, Text } from "react-native";
-import { SelectList, MultipleSelectList } from "react-native-dropdown-select-list";
-
-const App = () => {
-  const [selected, setSelected] = React.useState("");
-
-  const data = [
-    { value: "AMZN", key: "Amazon.com, Inc." },
-    { value: "AMD", key: "Advanced Micro Devices, Inc." },
-    { value: "AMC", key: "Tesla, Inc." },
-    { value: "TSLA", key: "England" },
-    { value: "META", key: "Meta Platforms, Inc." },
-    { value: "CS", key: "Codesmith.io" },
-  ];
-
-  return (
-    <View style={{ paddingHorizontal: 10, marginTop: 10 }}>
-      <SelectList setSelected={setSelected} data={data} />
-
-      <View style={{ marginTop: 50 }}>
-        <Text>Selected Value : </Text>
-        <Text style={{ marginTop: 10, color: "gray" }}>{selected}</Text>
-      </View>
-    </View>
-  );
-};
-
-export default App;
-
 // const styles = StyleSheet.create({
 //   container: {
 //     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
 //   },
 // });
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Root />
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+}
+
+const Root = () => {
+  return (
+    <Navigator>
+      <Screen name={"FIND STONKS"} component={Dropdown} />
+      <Screen name={"r/wallstreetbets"} component={StockInfo} />
+    </Navigator>
+  );
+};
